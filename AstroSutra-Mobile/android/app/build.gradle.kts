e09e9d0360d6155a5861fcd9a52f8cf43c45df59
Dixit-Anmol <bholudixitanmoldixit@gmail.com>
2026-07-25 16:32:49 +0530

    fix: bump compileSdk and targetSdk to 36 and minSdk to 21

--- AstroSutra-Mobile/android/app/build.gradle.kts
+++ AstroSutra-Mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.astrosutra_mobile"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,7 +20,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
